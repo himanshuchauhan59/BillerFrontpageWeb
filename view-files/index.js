ed19771e4fe279703b2c6bf985009e0be15f1f5b
Himanshu-1433 <himanshu1433@tnraocollege.org>
2023-07-05 21:33:30 +0530

    Save with half of the complete

--- view-files/index.js
+++ view-files/index.js
@@ -2,6 +2,22 @@ let toggleBol = false;
 let open_toggle = document.getElementById("open-toggle");
 let close_btn = document.getElementById("close_btn");
 let nav_item = document.querySelectorAll("li.nav-item");
+let card = document.querySelectorAll(".card");
+
+card.forEach((ele) => {
+    ele.childNodes[3].childNodes[1].addEventListener("click" , () => {
+        if(ele.childNodes[3].childNodes[1].style.transform == "") {
+            ele.childNodes[3].childNodes[1].style.transform = "rotate(180deg)";
+        }
+        else {
+            ele.childNodes[3].childNodes[1].style.transform = "";
+        }
+        
+        $(`#${ele.childNodes[5].childNodes[3].id}`).slideToggle();
+    })
+})
+
+
 nav_item.forEach((ele) => {
     ele.addEventListener("click" , () => {
         $(".slide-over").slideToggle();
